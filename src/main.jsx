d4549154c0228b5a5a3463e00538a4dcc2f25415
gabrielqt <gabriel.traiano4@gmail.com>
2024-12-12 11:42:36 -0300

    adjusting routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./assets/index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Home from "./pages/Home";
+import Base from "./pages/Base";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Base />,
+    children: [
+      {
+        path: "home",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
--- src/pages/Base.jsx
+++ src/pages/Base.jsx
@@ -1,5 +1,13 @@
+import { Outlet } from "react-router-dom";
+
 function Base() {
-  return <h1>hello world</h1>;
+  return (
+    <div>
+      <h1>oi teste</h1>
+      <p>teste</p>
+      <Outlet />
+    </div>
+  );
 }
 
 export default Base;
