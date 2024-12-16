90b9d9209351c3bdb80c67a8b36b38c0b3d4810c
gabrielqt <gabriel.traiano4@gmail.com>
2024-12-16 13:39:40 -0300

    grid homepage

--- src/components/ListCards.jsx
+++ src/components/ListCards.jsx
@@ -1,17 +1,27 @@
 import { experimentalStyled as styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid2";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: "#fff",
+// xs:  // (celulares)
+// sm:  //  (tablets)
+// md:  //  (PCs)
+
+const Item = styled("div")(({ theme }) => ({
   ...theme.typography.body2,
-  padding: theme.spacing(2),
   textAlign: "center",
-  color: theme.palette.text.secondary,
-  ...theme.applyStyles("dark", {
-    backgroundColor: "#1A2027",
-  }),
+  backgroundColor: "#f5f5f5", // Cor de fundo (pode ser ajustada)
+  padding: theme.spacing(2), // Espaçamento interno
+  borderRadius: "8px", // Borda arredondada
+  width: 130,
+  boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.9)",
+  transition: "0.2s ease all", // Sombra leve
+  "& img": {
+    width: 90,
+  },
+  "&:hover": {
+    cursor: "pointer",
+    transform: "scale(1.05)",
+  },
 }));
 
 export default function ListCards(props) {
@@ -19,13 +29,19 @@ export default function ListCards(props) {
     <Box sx={{ flexGrow: 1 }}>
       <Grid
         container
-        spacing={{ xs: 2, md: 3 }}
-        columns={{ xs: 4, sm: 8, md: 12 }}
+        spacing={{ xs: 2, md: 4 }}
+        columns={{ xs: 4, sm: 8, md: 20 }}
         sx={{ m: 2 }}
       >
         {props.brandSearch.map((brand) => (
-          <Grid key={brand.codigo} size={{ xs: 2, sm: 4, md: 4 }}>
-            <Item>{brand.nome}</Item>
+          <Grid
+            key={brand.codigo}
+            size={{ xs: 2, sm: 4, md: 4 }}
+            sx={{ display: "flex", justifyContent: "center" }}
+          >
+            <Item src="">
+              <img src={`/images/${brand.nome}.png`} />
+            </Item>
           </Grid>
         ))}
       </Grid>
